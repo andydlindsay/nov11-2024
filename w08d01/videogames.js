01ce9e74ce748a0d0bb6ef296403fb359be6554e
Andy Lindsay <andydlindsay@gmail.com>
2025-01-13 09:36:32 -0800

    add data

---
+++ w08d01/videogames.js
@@ -0,0 +1,28 @@
+const videoGames = {
+  "1": {
+    id: "1",
+    title: "The Legend of Zelda: Breath of the Wild",
+    releaseYear: 2017,
+    genre: "Action-adventure"
+  },
+  "2": {
+    id: "2",
+    title: "God of War (2018)",
+    releaseYear: 2018,
+    genre: "Action-adventure"
+  },
+  "3": {
+    id: "3",
+    title: "Minecraft",
+    releaseYear: 2011,
+    genre: "Sandbox"
+  },
+  "4": {
+    id: "4",
+    title: "The Witcher 3: Wild Hunt",
+    releaseYear: 2015,
+    genre: "Action RPG"
+  },
+};
+
+module.exports = videoGames;
